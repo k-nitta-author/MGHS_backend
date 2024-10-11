f00b149bd56c98f3328ac154a24074cb6779fcca
k-nitta-author <k.nitta.author@gmail.com>
2024-10-11 17:23:58 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -24,4 +24,4 @@ if __name__ == '__main__':
     environ['OPTIFLOW_ACCOUNTNAME'] = "optiflow.mghs@gmail.com"
     environ['OPTIFLOW_PASSWORD'] = "mhzz opbh fpdf kxgh"
 
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=10000)
