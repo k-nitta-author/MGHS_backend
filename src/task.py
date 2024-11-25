3602ded7138829ec3034bc3971f4793ac4108776
k-nitta-author <k.nitta.author@gmail.com>
2024-11-25 17:57:11 +0800

    Update task.py

--- src/task.py
+++ src/task.py
@@ -24,7 +24,7 @@ class TaskResource:
 
                 task : model = task
 
-                t: Team = Team.query.filter_by(id=task.team_id)
+                t: Team = Team.query.filter_by(id=task.team_id).all()
 
 
                 input = {
@@ -49,7 +49,7 @@ class TaskResource:
 
             task = model().query.filter_by(id=id).first()
 
-            t: Team = Team.query.filter_by(id=task.team_id)
+            t: Team = Team.query.filter_by(id=task.team_id).all()
 
 
             if not task:
