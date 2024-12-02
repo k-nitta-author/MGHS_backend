13d04692f4fd6c4b39de79f7698ec36812ff6e1a
k-nitta-author <k.nitta.author@gmail.com>
2024-12-02 19:12:56 +0800

    Update task.py

--- src/task.py
+++ src/task.py
@@ -36,7 +36,7 @@ class TaskResource:
                 if task.team_id:
                     t: Team = Team().query.filter_by(id=task.team_id).first()
 
-                    task_data["team_name"] = t.name
+                    input["team_name"] = t.name
 
                 output.append(input)
 
